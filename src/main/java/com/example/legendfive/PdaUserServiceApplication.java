0503446a9194343b70b38e6a711ccdfc9b24cce2
Subin <70842920+bin-pro@users.noreply.github.com>
2023-10-10 11:32:38 +0900

    Init: Initialize user service

---
+++ src/main/java/com/example/legendfive/PdaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.legendfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdaUserServiceApplication.class, args);
+	}
+
+}
